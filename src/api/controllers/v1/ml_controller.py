6d0cc90bd6471a0372d87933041c79241bde4b79
Iuri Volnistem <volnistem.developer@gmail.com>
2026-01-19 09:51:44 -0300

    fix: add protection to ML routes

--- src/api/controllers/v1/ml_controller.py
+++ src/api/controllers/v1/ml_controller.py
@@ -2,6 +2,7 @@
 from fastapi import APIRouter
 
 from src.api.config.dependency_injection_config import services
+from src.api.config.protected_security_decorator import protected
 from src.api.middlewares.auth_middleware import ProtectedRoute
 from src.dtos.ml_dto import MLFeaturesDTO, MLTrainingDTO
 
@@ -9,12 +10,14 @@ from src.dtos.ml_dto import MLFeaturesDTO, MLTrainingDTO
 router = APIRouter(prefix="/api/v1/ml", tags=["ML"], route_class=ProtectedRoute)
 
 @router.get("/features", status_code=200, response_model=list[MLFeaturesDTO])
+@protected()
 def features():
     service = services.get_ml_service()
 
     return service.get_ml_features()
 
 @router.get("/training-data", status_code=200, response_model=list[MLTrainingDTO])
+@protected()
 def training_data():
     service = services.get_ml_service()
 
